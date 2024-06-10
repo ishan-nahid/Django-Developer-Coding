db0b220114051e374eb76906dda83eb60c964c64
ishan-nahid <nahidboss96@gmail.com>
2024-06-10 11:07:46 +0600

    only model update done

--- src/product/admin.py
+++ src/product/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product, Variant, ProductImage, ProductVariant, ProductVariantPrice
+
+# Register your models here.
+admin.site.register(Product)
+admin.site.register(Variant)
+admin.site.register(ProductImage)
+admin.site.register(ProductVariant)
+admin.site.register(ProductVariantPrice)
